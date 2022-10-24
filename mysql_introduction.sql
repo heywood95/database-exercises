b635d39ab1e4c74ccd0a4da36804052f8e25d4fa
heywood95 <adam.heywood8@gmail.com>
2022-10-24 10:52:01 -0500

    adding mysql.introduction.sql

---
+++ mysql_introduction.sql
@@ -0,0 +1,8 @@
+select help_topic_id, help_category_id, url from mysql.help_topic;;
+select * from mysql.help_topic;;
+select user, host from mysql.user;;
+select * from mysql.user;;
+show create table orders;;
+use chipotle;;
+show databases;;
+show databases:;
